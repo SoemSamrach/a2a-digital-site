2150b89f66547757866bd3d70a3748864fac8f30
metheany <metheany.thorn@gmail..com>
2020-03-04 13:44:30 +0700

    pwa test 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from "../views/HomePage.vue";
 
 Vue.use(Router)
 
@@ -10,7 +11,7 @@ export default new Router({
         {
             path: '/',
             name: 'home',
-            component: () => import(/* webpackChunkName: "HomePage" */ '@/views/HomePage.vue')
+            component: Home
         },
         {
             path: '/solutiondetail/:solution',
